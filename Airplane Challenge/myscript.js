bdee73813c74e0592d094802433a021b3bf494f2
Philipp Dorner <pdorner@gmx.net>
2019-10-23 11:00:53 +0200

    airplain finished

--- Airplane Challenge/myscript.js
+++ Airplane Challenge/myscript.js
@@ -7,8 +7,20 @@ $(document).mousemove(function(e) {
     });
 });
 
-	$(`#moveme`).on(`click`, function(){
-		(this).toggleClass( "off" );
-
-	})
+	$(`#moveme`).on(`dblclick`, function(){
+		var a = $("#moveme");
+                a.hide(1000);
+                $.fx.speeds["very-slow"] = 2000;
+                a.show("very-slow");
+    $(document).off('mousemove');
 });
+	$(`#moveme`).on(`click`, function(){
+		$(document).mousemove(function(e) {
+    $('#moveme').offset({
+        left: e.pageX - 100,
+        top: e.pageY -100
+    });
+});});
+
+});
+	
